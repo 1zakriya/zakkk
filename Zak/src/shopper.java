fd81fc2f523d1d6486b89beb2e2d85a84a1d0c69
bca <bca@Bcalab4-m11>
2023-02-23 10:55:37 +0530

    gh

---
+++ Zak/src/shopper.java
@@ -0,0 +1,20 @@
+
+public class shopper {
+	int num_items;
+	double unit_cost;
+	String item_name;
+	double total_cost;
+	shopper(int num_items, double unit_cost, String item_name)
+	{
+		this.num_items=num_items;
+		this.unit_cost=unit_cost;
+		this.item_name=item_name;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+}
